a9b686d9f8f273cedfb239bc9b215143cec47ee1
Prajneya <prajneya.kumar@research.iiit.ac.in>
2020-04-03 17:19:57 +0530

    Fix Merge Conflicts

--- app.js
+++ app.js
@@ -117,10 +117,6 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-app.get("/success", function(req, res){
-  res.render("help-on-way");
-});
-
 app.get("/corona", function(req, res){
   res.render("corona");
 });
